54eb9df9644e4be7b6a941d6891bdde62b599dbc
Hamid Peiro <hamidpeiro@yahoo.com>
2026-06-21 19:01:52 +0200

    Build 1.0.1 packages with dialog suppression fix

--- RhinoIllustratorBridge/obj/Release/net7.0/RhinoIllustratorBridge.AssemblyInfo.cs
+++ RhinoIllustratorBridge/obj/Release/net7.0/RhinoIllustratorBridge.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyDescriptionAttribute(("Rhino ⇄ Adobe Illustrator Bridge – Import artboards, export curves, hatches, anno" +
     "tations and text"))]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b34f4c6dcbfbca53f0404883f534431a4d2390af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdbfe7a94354de07ecea90675ef57f0977f79a47")]
 [assembly: System.Reflection.AssemblyProductAttribute("RhinoIllustratorBridge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rhino Illustrator Bridge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
